df25096a5e9dce892f86c6fa1f3b5c8f8cc96524
Daniel Hayes <danielhayespco@gmail.com>
2018-06-14 04:28:55 -0500

    getting a basic skeleton

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HelloDiv from "./components/HelloDiv";
+import JSXVariables from "./components/JSXVariables";
+import Header from "./components/Header";
+import Section from "./components/Section";
+
+import Navbar from "./components/Navbar";
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <Section />
+  </div>
+  );
+export default App;
